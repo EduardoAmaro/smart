032a85c05e6a2610e6e97f047947d44550f271cc
EduardoAmaro <eduardo.rosa@melhorenvio.com>
2023-06-27 17:31:39 -0300

    feat: add whe to find banner inside 'fale conosco' page

---
+++ components/fale-conosco/WhereToFindBanner.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="section section-where-to-find">
+    <div class="container-fluid">
+      <div class="banner-wrapper">
+        <div class="image-banner">
+          <img alt="imagem" src="@/assets/images/wheretofind.jpg" />
+        </div>
+        <div class="text-banner">
+          <h4 class="uppertitle">Título superior</h4>
+          <h3 class="title">Onde nos encontrar</h3>
+          <p>comercial@smartabrasivos.com.br</p>
+          <p>(54) 3013-3040</p>
+          <p>
+            R. Natal Chiarelo, 481 - Desvio Rizzo - Caxias do Sul - RS - Brasil,
+            95012-663
+          </p>
+          <SocialLogos />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SocialLogos from '~/components/layout/SocialLogos'
+
+export default {
+  components: {
+    SocialLogos,
+  },
+}
+</script>
+
+<style lang="scss">
+.section-where-to-find {
+  margin-top: 120px;
+  margin-bottom: 240px;
+
+  .container-fluid {
+    padding: 0 25%;
+  }
+
+  .banner-wrapper {
+    display: flex;
+
+    .image-banner {
+      display: flex;
+      width: 50%;
+
+      img {
+        width: 100%;
+        object-fit: cover;
+        aspect-ratio: 1;
+      }
+    }
+
+    .text-banner {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      padding-left: 5%;
+      width: 50%;
+
+      .uppertitle {
+        text-transform: uppercase;
+        font-size: $font-size-md;
+      }
+
+      .title {
+        font-size: $font-size-lg;
+        margin-bottom: 27px;
+      }
+
+      p {
+        font-family: $font-family-title;
+        $font-size-md: 1rem;
+        margin-bottom: 20px;
+        max-width: 297px;
+      }
+
+      .social-logos {
+        a,
+        svg {
+          color: $text-color;
+          opacity: 1;
+          width: 30px;
+          height: 30px;
+          &:hover {
+            color: $orange-color;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
--- pages/fale-conosco.vue
+++ pages/fale-conosco.vue
@@ -5,17 +5,20 @@
       subtitle="Smart Abrasivos Industriais: soluções de alto desempenho para o seu negócio."
     />
     <FormContent />
+    <WhereToFindBanner />
   </main>
 </template>
 
 <script>
 import TopBanner from '~/components/empresa/TopBanner'
 import FormContent from '~/components/fale-conosco/FormContent'
+import WhereToFindBanner from '~/components/fale-conosco/WhereToFindBanner'
 
 export default {
   components: {
     TopBanner,
     FormContent,
+    WhereToFindBanner,
   },
 }
 </script>
